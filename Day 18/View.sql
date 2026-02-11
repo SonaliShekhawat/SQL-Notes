05d02f8d030a46bf6d14340ed8ac083e544ac298
SonaliShekhawat <shekhawat.sonali93@gmail.com>
2026-02-11 22:57:56 +0530

    views

---
+++ Day 18/View.sql
@@ -0,0 +1,36 @@
+# views
+# views are the virtual table define by stored select query
+# views do not stored data but provide a way to interact a data from 
+# base table.
+# syntax ---
+# create view viewname as
+# select columnlist/* from tablename
+
+# create view
+create view cust_details_usa as
+select * from customers where country="usa";
+
+select * from cust_details_usa;
+
+# create view with different column name
+create view cust_details(cnumber,cname,city,state,country) as
+select customername,customernumber,city,state,country from customers;
+
+select * from cust_details;
+
+# rename view
+rename table cust_details to customer_details;
+
+# drop view
+drop view customer_details;
+
+
+# create view from another view
+create view cust_details as
+select customername,customernumber,city,state,country from cust_details_usa;
+
+select * from cust_details;
+
+# join two views
+select count(*) from cust_details_usa cross join cust_details;
+
